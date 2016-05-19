bca7eb9f3fbfd802d3d3d15b4bb57a8ee5f39202
seth AMG <samegavie@webnet.fr>
2016-05-19 14:48:40 +0200

    htaccess

--- .mage/tasks/CdnTag.php
+++ .mage/tasks/CdnTag.php
@@ -30,23 +30,26 @@ class CdnTag extends AbstractTask
 			return false;
 		}
 
+		// create Htaccess no index
+		$commands[] = 'touch .htaccess && echo "Options -Indexes" > .htaccess';
+
 		// create if not exist
-		$commands[] = "mkdir -p $shared $tag";
+		$commands[] = "mkdir -p $shared $tag && cp .htaccess $tag && cp .htaccess $shared";
 
 		// copy directories shared and link in tag
-		$commands[] = "cp -r $src/assets/img $shared/ && ln -s $src/img $tag/img";
-		$commands[] = "cp -r $src/assets/plugins $shared/ && ln -s $src/plugins $tag/plugins";
-		$commands[] = "cp -r $src/assets/video $shared/ && ln -s $src/video $tag/video";
-		$commands[] = "cp -r $src/images $shared/ && ln -s $src/images $tag/images";
-		$commands[] = "cp -r $src/fonts $shared/ && ln -s $src/fonts $tag/fonts";
+		$commands[] = "cp -r $src/assets/img $shared/ && ln -s $src/assets/img $tag/img && cp .htaccess $tag/img";
+		$commands[] = "cp -r $src/assets/plugins $shared/ && ln -s $src/assets/plugins $tag/plugins && cp .htaccess $tag/plugins";
+		$commands[] = "cp -r $src/assets/video $shared/ && ln -s $src/assets/video $tag/video && cp .htaccess $tag/video";
+		$commands[] = "cp -r $src/images $shared/ && ln -s $src/images $tag/images && cp .htaccess $tag/images";
+		$commands[] = "cp -r $src/fonts $shared/ && ln -s $src/fonts $tag/fonts && cp .htaccess $tag/fonts";
 
 		// copy directories tag
-		$commands[] = "cp -r $src/assets/ajax $tag/";
+		$commands[] = "cp -r $src/assets/ajax $tag/ && cp .htaccess $tag/ajax";
 		$commands[] = "mkdir -p $tag/css/private";
 		$commands[] = "mkdir -p $tag/js/private";
-		$commands[] = "cp -r $src/assets/css $tag";
-		$commands[] = "cp -r $src/css/* $tag/css/private";
-		$commands[] = "cp -r $src/assets/js $tag";
+		$commands[] = "cp -r $src/assets/css $tag && cp .htaccess $tag/css";
+		$commands[] = "cp -r $src/css/* $tag/css/private && cp .htaccess $tag/css/private";
+		$commands[] = "cp -r $src/assets/js $tag && cp .htaccess $tag/js";
 //		$commands[] = "cp -r $src/js/* $tag/js/private";
 
 		$commands[] = "sudo chmod -R 775 $shared";
