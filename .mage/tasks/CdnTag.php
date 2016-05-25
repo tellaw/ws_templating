428bda38fcdf3d39beeefdc0a5ca045f24aadefb
seth AMG <samegavie@webnet.fr>
2016-05-25 13:51:09 +0200

    correction script deploiement cdn and dev

--- .mage/tasks/CdnTag.php
+++ .mage/tasks/CdnTag.php
@@ -22,7 +22,6 @@ class CdnTag extends AbstractTask
 
 		$tmpl_dir = "/data/apps/eti_cdn/lucy/ti";
 		$src = $tmpl_dir."/app/current/web";
-		$shared = $tmpl_dir."/shared";
 		$tag = $tmpl_dir."/".getenv("CDN_TAG");
 
 		if (empty($tag)) {
@@ -34,20 +33,20 @@ class CdnTag extends AbstractTask
 		$commands[] = 'touch .htaccess && echo "Options -Indexes" > .htaccess';
 
 		// create if not exist
-		$commands[] = "mkdir -p $shared $tag && cp .htaccess $tag && cp .htaccess $shared";
+		$commands[] = "mkdir -p $tag/ && cp .htaccess $tag/";
 
 		// copy directories shared and link in tag
-		$commands[] = "cp -r $src/assets/* $shared/ && ln -s $src/assets $tag/assets && cp .htaccess $tag/assets";
-		$commands[] = "cp -r $src/css $tag";
-		$commands[] = "cp -r $src/fonts $tag";
-		$commands[] = "cp -r $src/images $tag";
+		$commands[] = "cp -r $src/assets $tag/";
+		$commands[] = "cp -r $src/css $tag/";
+		$commands[] = "cp -r $src/fonts $tag/";
+		$commands[] = "cp -r $src/images $tag/";
 
-		$commands[] = "ln -s $tag $tmpl_dir/latest";
-		$commands[] = "sudo chmod -R 775 $tmpl_dir";
+		$commands[] = "rm $tmpl_dir/latest && ln -s $tag $tmpl_dir/latest";
+		$commands[] = "sudo chmod -R 775 $tmpl_dir/";
 
 		// Execute commands
 		foreach ($commands as $command) {
-			// And run
+			echo "Command >> ".$command;
 			if (!$this->runCommandRemote($command)) return false;
 		}
 		return true;
