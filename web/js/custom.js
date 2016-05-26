59fb8f84c871caee11c7a21dd3fa33c9904a4f29
fguerard <fguerard@weka.fr>
2016-05-26 17:43:55 +0200

    modif $ en jQuery

--- web/js/custom.js
+++ web/js/custom.js
@@ -1,13 +1,13 @@
 // Home javascript
 
-$( window ).resize(function() {
+jQuery( window ).resize(function() {
     if ($( "#sub-menu" ).hasClass( "fxd" )) {
         $('#sub-menu').css('margin-top', $('#main-header').height());
     } else if (!$( "#sub-menu" ).hasClass( "fxd" )) {
     }
 });
 
-$( window ).ready(function() {
+jQuery( window ).ready(function() {
     if ($( "#sub-menu" ).hasClass( "fxd" )) {
         $('#sub-menu').css('margin-top', $('#main-header').height());
     } else if (!$( "#sub-menu" ).hasClass( "fxd" )) {
