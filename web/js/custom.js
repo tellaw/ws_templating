df68bc7f58c117a122a0c5ac2e89ed601ee21344
fguerard <fguerard@weka.fr>
2016-06-20 15:26:09 +0200

    popin sur les webinars

--- web/js/custom.js
+++ web/js/custom.js
@@ -297,9 +297,11 @@ jQuery(document).ready(function($) {
     });
 
     //popin webinar
-    $("#di-conf-popin").dialog("widget").attr("id", "design-di-conf-popin").css("position", "fixed");
-    $("#di-conf-popin").dialog("widget").css("top", "70px");
-    $("#design-di-conf-popin").css("left", (($(window).width()-$("#design-di-conf-popin").width())/2));
+    $(".iframe-conf").click(function() {
+        $("#di-conf-popin").dialog("widget").attr("id", "design-di-conf-popin").css("position", "fixed");
+        $("#di-conf-popin").dialog("widget").css("top", "70px");
+        $("#design-di-conf-popin").css("left", (($(window).width()-$("#design-di-conf-popin").width())/2));
+    });
 });
 
 (function() {
