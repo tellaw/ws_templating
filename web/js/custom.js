15b7329eef3f0b5bf6dc3fdc38156f5201f9a77c
fguerard <fguerard@weka.fr>
2016-07-08 10:49:33 +0200

    correction recette (accordion)

--- web/js/custom.js
+++ web/js/custom.js
@@ -414,6 +414,15 @@ jQuery(document).ready(function() {
         jQuery('.tout-ouvrir').text('Tout ouvrir');
     });
 
+    $('#accordion-1').on('shown.bs.collapse', function (e) {
+        var offset = $(this).find('.collapse.in').prev('.panel-heading');
+        if(offset) {
+            $('html,body').animate({
+                scrollTop: $(offset).offset().top -180
+            }, 500);
+        }
+    });
+
     jQuery(".zoning")
         .click(
             function(a){
