f6ec7f62b43bad38a08544d6ba3b2ce2a8438b8f
amahier <alain.mahier@gmail.com>
2016-09-22 14:38:22 +0200

    ST-6436 - Transfert des js sur la page de recherche ici
    Initialisation des datepiker de la page

--- web/js/search/advanced-search.js
+++ web/js/search/advanced-search.js
@@ -1,4 +1,47 @@
 jQuery(document).ready(function ($) {
+	
+	/*affichage box favoris en hover sur "favoris de recherche" dans la page de recherche"*/
+	$("#wrap-favoris").hover(function() {
+		$("#boxFavoris").show();
+	},function(){ 
+		$("#boxFavoris").hide();
+	});
+	
+	$("#boxFavoris").hover(function() {
+		$(this).show();
+	},function(){ 
+		$(this).hide();
+	});
+
+	/*affichage box historique en hover sur "historique récente" dans la page de recherche"*/
+	$("#wrap-history").hover(function() {
+		$("#boxHistory").show();
+	},function(){ 
+		$("#boxHistory").hide();
+	});
+	
+	$("#boxHistory").hover(function() {
+		$(this).show();
+	},function(){ 
+		$(this).hide();
+	});
+	
+	function displayStateForFacetCheckbox(facetName) {
+		var isOneChecked=false;
+		var isAllChecked=true;
+
+		$( "#" + facetName + "-all" ).closest("li").find("input[data-"+facetName+"-filter]").each(function(index) {
+			isOneChecked |= $(this).prop("checked");
+			isAllChecked &= $(this).prop("checked");
+		});
+		
+		if (isOneChecked) {
+			$( "#" + facetName + "-all").prop('checked', true);
+		} else {
+			$( "#" + facetName + "-all").prop('unchecked', true);
+		}
+	}
+	
 	$("input[data-filter-all]").on("click", function() {
 		var paramName = $(this).data("filter-all");
 		$(this).prop("checked", function (i, old) {
@@ -25,6 +68,7 @@ jQuery(document).ready(function ($) {
 	
 	$("input[data-filter-all]").each(function (i, box) {
 		var paramName = $(box).data("filter-all");
+		displayStateForFacetCheckbox(paramName);
 		$("input[data-"+paramName+"-filter]").on("click", function() {
 			$(this).prop("checked", function(i, old) {
 				var mask = $(this).data(paramName+"-filter");
@@ -58,4 +102,40 @@ jQuery(document).ready(function ($) {
 		});
 
 	});
+	
+	$("input[data-ignore-if-empty]").on("change", function() {
+		if ($(this).val().trim() === "") {
+			$(this).removeAttr("name");
+		} else {
+			$(this).attr("name", $(this).data("field-name"));
+		}
+	});
+	
+	// Date range
+	
+	// gestion datepicker
+    // Date range
+	$.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );	
+    $('#avant-picker').datepicker({
+        altField: "#avant",
+        altFormat: 'dd/mm/yy',
+        prevText: "<i class='fa fa-angle-left font-size-25'></i>",
+        nextText: "<i class='fa fa-angle-right font-size-25'></i>",
+        changeYear: true,
+        onSelect: function( selectedDate ) {
+            $('#apres-picker').datepicker('option', 'minDate', selectedDate);
+            $("#avant").change(); // to have input name set
+        }
+    });
+    $('#apres-picker').datepicker({
+        altField: "#apres",
+        altFormat: 'dd/mm/yy',
+        //prevText: '<i class="fa fa-angle-left font-size-25"></i>',
+        //nextText: '<i class="fa fa-angle-right font-size-25"></i>',
+        changeYear: true,
+        onSelect: function( selectedDate ) {
+            $('#avant-picker').datepicker('option', 'maxDate', selectedDate);
+            $("#apres").change(); // to have input name set
+        }
+    });
 })
