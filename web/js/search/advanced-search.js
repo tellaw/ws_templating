65ed5313213f20373ab9a74fd1a5b4feb74e1df9
amahier <alain.mahier@gmail.com>
2016-10-11 15:18:12 +0200

    ST-6659 - Lucy - Recherche
    Fix reset filters

--- web/js/search/advanced-search.js
+++ web/js/search/advanced-search.js
@@ -77,11 +77,9 @@ jQuery(document).ready(function ($) {
 			}
 		});
 		$("#"+paramName+"-toggle").on("click", function() {
-			$(this).prop("checked", function (i, old) {
-				var tv = $(this).data("toggle-value");
-				$(this).data("toggle-value", $(this).val());
-				$(this).val(tv);
-			})
+			var tv = $(this).data("toggle-value");
+			$(this).data("toggle-value", $(this).val());
+			$(this).val(tv);
 		});
 		
 	});
