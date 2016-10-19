00b8557937fc90c8557bb02561e8b62842cc4c88
fguerard <fguerard@weka.fr>
2016-10-19 15:58:30 +0200

    coprs article

--- web/js/custom.js
+++ web/js/custom.js
@@ -268,7 +268,7 @@ jQuery(document).ready(function() {
     jQuery("#menu-smint > .nav-tabs a").on("click", function(){
         var link = window.location.href;
         var anchor = jQuery(this).attr("href");
-
+        console.log(anchor);
         if (anchor.indexOf("#") > 0) {
             //affichage ou non de la freezone en col de droite
             if (anchor == "#offre-commerciale" || anchor == "#details") {
