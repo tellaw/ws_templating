dd3c0f2f19ab306359fe9ef989911b30f1b83408
fguerard <fguerard@weka.fr>
2017-02-10 12:02:11 +0100

    correction recette

--- web/js/custom.js
+++ web/js/custom.js
@@ -870,7 +870,7 @@ $(document).ready(function () {
 
     // ajout class et attributs sur les liens du menu
     $("#menu-menu-generique-ti > li").each(function(){
-        $(this).find("a").addClass("dropdown-toggle").attr("data-toggle","dropdown");
+        $("#menu-menu-generique-ti > li > a").addClass("dropdown-toggle").attr("data-toggle","dropdown");
     });
 });
 (function () {
