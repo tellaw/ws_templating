e887e3b152b7fe0322df499221e71a6c903b9de4
ingrid <iprotain@e-biz.fr>
2017-03-06 15:05:39 +0100

    js pour mon compte

---
+++ web/js/myti.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    $('#leadDepartment').change(function(evt){
+		ajaxOnChangedLeadDepartment($(this).val());
+    });
+
+	function initPictureForm () {
+
+		var navigatorName;
+		navigatorName = navigator.appName.toLowerCase();
+
+		if (navigatorName.indexOf("internet explorer") == -1) {
+
+			<#--
+				Not IE navigator => Activate JS on form
+			 -->
+			jQuery("#picture-button").click(function () {
+				jQuery('#picture-input').click();
+			});
+
+			var fileInput = document.getElementById("picture-input");
+			fileInput.onchange = function () {changeListenerOnFileInput()};
+
+
+			<#--
+				CSS
+			-->
+			jQuery('#picture-input').css("display", "none");
+			jQuery("#picture-button").css("display", "block");
+			jQuery("#picture-button-ie").css("display","none");
+			jQuery("#picture-form-submit").css("display", "none");
+			jQuery('p.red').css("display", "none");
+		}
+	}
+
+	function changeListenerOnFileInput () {
+
+		var pictureForm, pictureInput, file, iSize, mbSize;
+		pictureForm = document.forms["picture-form"];
+		pictureInput = pictureForm.elements["picture"];
+		file = pictureInput.files[0];
+		if (file) {
+			iSize = file.size / 1024;
+			mbSize = (Math.round((iSize / 1024) * 100) / 100);
+
+			if (mbSize > 2){
+				jQuery("#picture-input-size span").html("Le fichier est trop volumineux. Veuillez en choisir un autre");
+				jQuery("#picture-input-size").css("display", "block");
+				$("#picture-input").attr("value","");
+		     } else {
+				jQuery("#picture-input-size").html("");
+				jQuery("#picture-form-submit").click();
+				jQuery("#picture-input-size").css("display", "none");
+			}
+		}
+	}
+
+	initPictureForm ();
+
+});
+
+function ajaxOnChangedLeadDepartment(departmentId) {
+	jQuery.ajax({
+		url : '/ajax/lucy/myti/profile/ajaxOnChangedLeadService?leadDepartmentId=' + encodeURIComponent(departmentId),
+		success : function (data) {
+			var leadFunction = $('#leadFunction');
+            leadFunction.empty();
+            for (var i = 0; i < data.length; i++) {
+                leadFunction.append('<option value=' + data[i].value + '>' + data[i].name + '</option>');
+            }
+		}
+	});
+	return false;
+}
