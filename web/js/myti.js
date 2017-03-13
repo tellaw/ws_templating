03d65300468eba370a6fe05e7ddf99586b30bec8
ingrid <iprotain@e-biz.fr>
2017-03-13 16:18:25 +0100

    Mon compte : ST-7079 : menu bloc espace admin hide if no entry

--- web/js/myti.js
+++ web/js/myti.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
-  //On cache le bloc #mon-espace-admin s'il n'a pas d'entrée 
-  if (jQuery("#mon-espace-admin").has( "ul" ).length == 0) {
-    jQuery("#mon-espace-admin").hide();
+  //On cache le bloc #mon-espace-admin s'il n'a pas d'entrée
+  if (jQuery("#mon-espace-admin").has( "ul" ).length > 0) {
+    jQuery("#mon-espace-admin").show();
   }
 });
