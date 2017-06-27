0386d408690294f26987cc0a8da1d641692829b5
fguerard <fguerard@weka.fr>
2017-06-27 17:24:27 +0200

    corrections pour pubs

--- web/js/custom.js
+++ web/js/custom.js
@@ -45,19 +45,49 @@ $(document).ready(function () {
     //affichage interstitiel
     $( "#div-gpt-ad-1490604501599-9 iframe" ).load(function() {
         var iBody = $(this).contents().find("body");
+        console.log(iBody);
         if(iBody.text().length != 1) {
             $("#overlaypub").show();
         }
     });
     //affichage banner bottom
-    $( "#pub_fixed_footer div[id^='div-gpt-ad-1490604501599-']").each(function() {
-        $( "iframe" ).load(function() {
+    var pubOk = 0;
+    if(pubOk == 0){
+        $( "#pub_fixed_footer #div-gpt-ad-1490604501599-0 iframe" ).load(function() {
             var iBody = $(this).contents().find("body");
+            var widthPub = $(this).width();
+            var heightPub = $(this).height();
             if(iBody.text().length != 1) {
+                pubOk = 1;
                 $("#pub_fixed_footer").show();
+                $("#pub_fixed_footer #pubclose p").css({"left" : widthPub/2, "bottom" : heightPub + 10}).show();
             }
         });
-    });
+    }
+    else if(pubOk == 0) {
+        $("#pub_fixed_footer #div-gpt-ad-1490604501599-4 iframe").load(function () {
+            var iBody = $(this).contents().find("body");
+            var widthPub = $(this).width();
+            var heightPub = $(this).height();
+            if (iBody.text().length != 1) {
+                pubOk = 1;
+                $("#pub_fixed_footer").show();
+                $("#pub_fixed_footer #pubclose p").css({"left": widthPub / 2, "bottom": heightPub + 10}).show();
+            }
+        });
+    }
+    else if(pubOk == 0) {
+        $("#pub_fixed_footer #div-gpt-ad-1490604501599-2 iframe").load(function () {
+            var iBody = $(this).contents().find("body");
+            var widthPub = $(this).width();
+            var heightPub = $(this).height();
+            if (iBody.text().length != 1) {
+                pubOk = 1;
+                $("#pub_fixed_footer").show();
+                $("#pub_fixed_footer #pubclose p").css({"left": widthPub / 2, "bottom": heightPub + 10}).show();
+            }
+        });
+    }
 
     // add a hash to the URL when the user clicks on a tab
     $('a[data-toggle="tab"]').on('click', function (e) {
