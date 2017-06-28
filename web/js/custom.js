5cdff6d829db3421bc5022351eea76ee8d453649
fguerard <fguerard@weka.fr>
2017-06-28 11:20:53 +0200

    corrections pour pubs

--- web/js/custom.js
+++ web/js/custom.js
@@ -45,54 +45,23 @@ $(document).ready(function () {
 
         //affichage interstitiel
         $( "#div-gpt-ad-1490604501599-9 iframe" ).load(function() {
-            var iBody = $(this).contents().find("body").html();
-            if(iBody.length > 1) {
+            var iBody = $(this).contents().find("body");
+            console.log(iBody.length);
+            if(iBody.length == 1) {
                 $("#overlaypub").show();
             }
         });
     }
 
     //affichage banner bottom
-    var pubOk = 0;
-    if(pubOk == 0){
-        $( "#pub_fixed_footer #div-gpt-ad-1490604501599-0 iframe" ).load(function() {
-            var iBody = $(this).contents().find("body").html();
-            var widthPub = $(this).width();
-            var heightPub = $(this).height();
-            if(iBody.length > 1) {
-                console.log(iBody.length);
-                pubOk = 1;
-                $("#pub_fixed_footer").show();
-                $("#pub_fixed_footer #pubclose p").css({"left" : widthPub/2, "bottom" : heightPub + 10}).show();
-            }
-        });
-    }
-    else if(pubOk == 0) {
-        $("#pub_fixed_footer #div-gpt-ad-1490604501599-4 iframe").load(function () {
-            var iBody = $(this).contents().find("body").html();
-            var widthPub = $(this).width();
-            var heightPub = $(this).height();
-            if (iBody.length > 1) {
-                console.log(iBody.length);
-                pubOk = 1;
-                $("#pub_fixed_footer").show();
-                $("#pub_fixed_footer #pubclose p").css({"left": widthPub / 2, "bottom": heightPub + 10}).show();
-            }
-        });
-    }
-    else if(pubOk == 0) {
-        $("#pub_fixed_footer #div-gpt-ad-1490604501599-2 iframe").load(function () {
-            var iBody = $(this).contents().find("body").html();
-            var widthPub = $(this).width();
-            var heightPub = $(this).height();
-            if (iBody.length > 1) {
-                console.log(iBody.length);
-                pubOk = 1;
-                $("#pub_fixed_footer").show();
-                $("#pub_fixed_footer #pubclose p").css({"left": widthPub / 2, "bottom": heightPub + 10}).show();
-            }
-        });
-    }
+    $( "#pub_fixed_footer #div-gpt-ad-1490604501599-0 iframe" ).load(function() {
+        var iBody = $(this).contents().find("body").html();
+        var widthPub = $(this).width();
+        var heightPub = $(this).height();
+        if(iBody.length > 1) {
+            $("#pub_fixed_footer #pubclose p").css({"left" : widthPub/2, "bottom" : heightPub - 10}).show();
+        }
+    });
 
     // add a hash to the URL when the user clicks on a tab
     $('a[data-toggle="tab"]').on('click', function (e) {
